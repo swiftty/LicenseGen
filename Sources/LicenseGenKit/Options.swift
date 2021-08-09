bc350b168de3431e414f185872b4b1e7aa74267f
swiftty <62803132+swiftty@users.noreply.github.com>
2021-08-09 22:10:35 +0900

    load yaml file

--- Sources/LicenseGenKit/Options.swift
+++ Sources/LicenseGenKit/Options.swift
@@ -9,21 +9,40 @@ public struct Options {
 
     public var outputFormat: OutputFormat
 
+    public var config: Config?
+
     public init(
         checkoutsPaths: [URL],
         packagePaths: [URL] = [],
         outputPath: URL?,
-        outputFormat: OutputFormat
+        outputFormat: OutputFormat,
+        config: Config? = nil
     ) {
         self.checkoutsPaths = checkoutsPaths
         self.packagePaths = packagePaths
         self.outputPath = outputPath
         self.outputFormat = outputFormat
+        self.config = config
     }
 }
 
 extension Options {
     public enum OutputFormat {
         case settingsBundle(prefix: String)
+    }
+
+    public struct Config {
+        public var modifiers: [String: Setting]
+
+        public enum Setting {
+            case ignore
+            case licensePath(String)
+        }
+
+        public init(
+            modifiers: [String: Setting]
+        ) {
+            self.modifiers = modifiers
+        }
     }
 }
--- Sources/licensegen/main.swift
+++ Sources/licensegen/main.swift
@@ -1,5 +1,6 @@
 import Foundation
 import ArgumentParser
+import Yams
 import Logging
 import LicenseGenKit
 
@@ -10,10 +11,14 @@ struct LicenseGenCommand: ParsableCommand {
         static func help(for value: LicenseGenCommand.OutputFormat) -> ArgumentHelp? {
             switch value {
             case .settingsBundle:
-                return "plist style for iOS"
+                return "plist style for application"
             }
         }
     }
+
+    @Option(name: .long,
+            help: "You can specify config path. Default: .licensegen.yml")
+    var configPath: String?
 
     @Option(name: .long,
             help: "You can specify custom spm checkouts path. Default: ${BUILD_DIR}/../../SourcePackages/checkouts",
@@ -42,11 +47,42 @@ struct LicenseGenCommand: ParsableCommand {
         let options = Options(checkoutsPaths: try extractCheckoutPaths(),
                               packagePaths: packagePaths.map(URL.init(fileURLWithPath:)),
                               outputPath: outputPath.map(URL.init(fileURLWithPath:)),
-                              outputFormat: try extractOutputFormat())
+                              outputFormat: try extractOutputFormat(),
+                              config: try extractConfig())
 
         LoggingSystem.bootstrap(StreamLogHandler.standardError)
         let logger = Logger(label: "lisencegen")
         try LicenseGen(logger: logger).run(with: options)
+    }
+
+    private func extractConfig() throws -> Options.Config? {
+        func extractConfigPath() -> String? {
+            if configPath == nil {
+                let path = ".licensegen.yml"
+                if FileManager.default.fileExists(atPath: path) {
+                    return path
+                }
+                return nil
+            }
+            return configPath
+        }
+        guard let configPath = extractConfigPath() else { return nil }
+        let data = try Data(contentsOf: URL(fileURLWithPath: configPath))
+        guard case let configs = try YAMLDecoder().decode([String: Config].self, from: data),
+              !configs.isEmpty else { return nil }
+
+        return try Options.Config(modifiers: configs.compactMapValues {
+            if $0.ignore.value && $0.licensePath != nil {
+                throw ValidationError("You can only specify ignore: or license_path:")
+            }
+            if $0.ignore.value {
+                return .ignore
+            } else if let path = $0.licensePath {
+                return .licensePath(path)
+            } else {
+                return nil
+            }
+        })
     }
 
     private func extractCheckoutPaths() throws -> [URL] {
@@ -72,6 +108,30 @@ struct LicenseGenCommand: ParsableCommand {
                 return .settingsBundle(prefix: prefix)
             }
             throw ValidationError("Missing expected argument '--settings-bundle-prefix <settings-bundle-prefix>'")
+        }
+    }
+}
+
+extension LicenseGenCommand {
+    struct Config: Decodable {
+        var ignore: Ignore
+        var licensePath: String?
+
+        private enum CodingKeys: String, CodingKey {
+            case ignore
+            case licensePath = "license_path"
+        }
+
+        struct Ignore: Decodable {
+            var value: Bool
+            init(from decoder: Decoder) throws {
+                let container = try decoder.singleValueContainer()
+                if container.decodeNil() {
+                    value = true
+                    return
+                }
+                value = try container.decode(Bool.self)
+            }
         }
     }
 }
