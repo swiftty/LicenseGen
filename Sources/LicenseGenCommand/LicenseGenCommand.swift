da136f393896fc7b78e34ce6478e82d5a6dd6424
swiftty <62803132+swiftty@users.noreply.github.com>
2021-08-14 18:17:09 +0900

    catch error and logging location

--- Sources/LicenseGenCommand/LicenseGenCommand.swift
+++ Sources/LicenseGenCommand/LicenseGenCommand.swift
@@ -10,15 +10,15 @@ public struct LicenseGenCommand: ParsableCommand {
     public init() {}
 
     public mutating func run() throws {
-        let options = Options(checkoutsPaths: try options.checkoutsPaths().map(\.url),
-                              packagePaths: options.packagePaths.map(\.url),
-                              outputPath: options.outputPath?.url,
-                              outputFormat: try options.outputFormat(),
-                              perProducts: options.perProducts,
-                              config: try options.config ?? .default())
-
         LoggingSystem.bootstrap(StreamLogHandler.standardError)
         let logger = Logger(label: "lisencegen")
-        try LicenseGen(logger: logger).run(with: options)
+        try LicenseGen(logger: logger).run(
+            with: Options(checkoutsPaths: try options.checkoutsPaths().map(\.url),
+                          packagePaths: options.packagePaths.map(\.url),
+                          outputPath: options.outputPath?.url,
+                          outputFormat: try options.outputFormat(),
+                          perProducts: options.perProducts,
+                          config: try options.config ?? .default())
+        )
     }
 }
