7e5f51c710fa200e8c65e64fe0272a49e458ad32
swiftty <62803132+swiftty@users.noreply.github.com>
2022-03-26 17:06:31 +0900

    update dependencies

--- Package.swift
+++ Package.swift
@@ -14,15 +14,15 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser",
-                 "0.3.0"..<"0.5.0"),
-
-        .package(url: "https://github.com/jpsim/Yams.git",
-                 from: "4.0.0"),
-
-        .package(url: "https://github.com/apple/swift-log.git",
                  from: "1.0.0"),
 
-        .package(url: "https://github.com/apple/swift-tools-support-core.git",
+        .package(url: "https://github.com/jpsim/Yams",
+                 from: "5.0.0"),
+
+        .package(url: "https://github.com/apple/swift-log",
+                 from: "1.0.0"),
+
+        .package(url: "https://github.com/apple/swift-tools-support-core",
                  from: "0.2.0")
     ],
     targets: [
